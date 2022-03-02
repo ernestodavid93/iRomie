2d8ed16d5c57ea896fbcfe662d30319e1dc9843b
Ernesto David <locodecumbia@gmail.com>
2022-03-02 17:00:10 -0600

    add user

--- assets/js/registerUser.js
+++ assets/js/registerUser.js
@@ -1,11 +1,19 @@
-document.getElementById("btnLogin").addEventListener("click", function(e){
+document.getElementById("btnS").addEventListener("click", function(e){
 
     let username = document.getElementById("username");
     let password = document.getElementById("password");
+    let nombre = document.getElementById("nombre");
+    let apellido = document.getElementById("apellido");
+    let telefono = document.getElementById("telefono");
+    let descripcion = document.getElementById("descripcion");
   
 
     console.log(username.value);
     console.log(password.value);
+    console.log(nombre.value);
+    console.log(apellido.value);
+    console.log(telefono.value);
+    console.log(descripcion.value);
     
 
     ///// El siguiente código comprueba un usuario y pass mediante un POST
@@ -13,11 +21,14 @@ const data = {
   
   username: username.value,
   password: password.value,
- 
+  nombre: nombre.value,
+  apellido: apellido.value,
+  telefono: telefono.value,
+  descripcion: descripcion.value,
   
   };
 
-fetch("http://localhost:8081/api/login/", {
+fetch("http://localhost:8081/api/user/", {
 method: 'POST', // or 'PUT'
 headers: {
   'Content-Type': 'application/json',
